faa673e1fec8998059a52a3e0e8c7f3c92d0ed35
JuanjoAvila <juanjo.avila.chavero@gmail.com>
2019-02-10 20:46:43 +0100

    Añadiendo la implementacion de using y dispose

---
+++ UsingDisposeExercice/UsingDisposeExercice/DisposeExercice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingDisposeExercice
+{
+    class DisposeExercice : IDisposable
+    {
+        public string Example { get; set; }
+
+        public DisposeExercice(String ex)
+        {
+            this.Example = ex;
+        }
+
+        void IDisposable.Dispose()
+        {
+            // Method intentionally left empty.
+        }
+
+        public void DisposeFunction() => Console.WriteLine("Se ha llamado al metodo Dispose()");
+    }
+}
---
+++ UsingDisposeExercice/UsingDisposeExercice/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingDisposeExercice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UsingMethod();
+            DisposeMethod();
+        }
+        static void UsingMethod()
+        {
+            String example ="Hola con using";
+            using (DisposeExercice exercice = new DisposeExercice(example))
+            {
+                Console.WriteLine($"Usando using: {exercice}");
+                Console.Read();
+            }
+
+        }
+        static void DisposeMethod()
+        {
+            String example = "Hola con dispose";
+            DisposeExercice exercice = new DisposeExercice(example);
+            exercice.DisposeFunction();
+            Console.WriteLine($"Usando dispose{exercice}");
+            Console.Read();
+        }
+    }
+}
